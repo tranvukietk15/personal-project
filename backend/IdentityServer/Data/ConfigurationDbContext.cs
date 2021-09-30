b83d8dad0ea2a47eca5b2e1670f2ce1748431279
kiettran <tranvukietk15@gmail.com>
2021-09-30 22:34:00 +0700

    rename project

---
+++ backend/IdentityServer/Data/ConfigurationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class ConfigDbContext : ConfigurationDbContext
+    {
+        public ConfigDbContext(DbContextOptions<ConfigurationDbContext> options, ConfigurationStoreOptions storeOptions) : base(options, storeOptions)
+        {
+        }
+    }
+}
